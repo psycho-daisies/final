286c011bc8ecacd801e67c6f3dc6694a1b1bb6fc
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-03-19 17:46:14 -0700

    Create binary_search_tree.py

---
+++ code_library/binary_search_tree.py
@@ -0,0 +1,7 @@
+"""
+Troy Brunette
+Final - Code Library - Binary Search Tree
+
+Binary Search Tree / AVL
+Implement a pre-order, post-order, and in-order, along with an AVL to balance your tree.
+"""
