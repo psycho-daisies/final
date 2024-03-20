efeb3b1b83e00753422270a04cb233659bcc06d5
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-03-19 17:44:24 -0700

    Create exponent.py

---
+++ recursion/exponent.py
@@ -0,0 +1,10 @@
+"""
+Troy Brunette
+CS240
+Recursion: Exponents
+
+Implement a solution using recursion
+where the user input a value (x), an exponent (e) and the method generates the result.
+
+Now compare the efficiency of each (iteration versus recursion).  What is the efficiency of each.
+"""
