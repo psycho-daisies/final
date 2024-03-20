3bee15ff84ccf8206a74c90f15ea4c2b48d6075a
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-03-20 10:42:43 -0700

    Update exponent.py

--- recursion/exponent.py
+++ recursion/exponent.py
@@ -1,7 +1,7 @@
 """
 Troy Brunette
 CS240
-Recursion: Exponents
+Final - Recursion: Exponents
 
 Implement a solution using recursion
 where the user input a value (x), an exponent (e) and the method generates the result.
