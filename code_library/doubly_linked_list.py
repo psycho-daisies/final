00919014baf34fa0ec011b7829d9d23e4b5a64f6
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-03-19 17:45:58 -0700

    Create doubly_linked_list.py

---
+++ code_library/doubly_linked_list.py
@@ -0,0 +1,35 @@
+"""
+
+Code Library (50 points)
+You have been building a code library in the programming language of your choice through the homework. Please submit this library.
+Data Structures
+You will not use built-in implementations of these data structures.
+
+Double Linked List
+• Head
+• Node
+    ○ Data
+    ○ Next
+    ○ Prev
+• Read (beginning, random position, end)
+• Insert (beginning, random position, end)
+• Delete (beginning, random position, end)
+
+Binary Search Tree / AVL
+Implement a pre-order, post-order, and in-order, along with an AVL to balance your tree.
+
+Graphs
+Implement a Breadth First Search and a Depth First Search and stack for both a Linked List & Array.
+You can use a doubly linked list.
+
+Search Algorithms  (Iterative & Recursive versions)
+    • Linear Search
+    • Binary Search
+
+Sorting Algorithms (for Arrays and Linked Lists)
+    • Selection Sort
+    • Insertion Sort
+    • Quick Sort
+    • Merge Sort
+
+"""
