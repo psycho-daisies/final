48c8b9b38a04b6be00ba77e07bd4a06490eb7f59
Troy <130083210+psycho-daisies@users.noreply.github.com>
2024-03-19 17:44:48 -0700

    Create lcd.py

---
+++ recursion/lcd.py
@@ -0,0 +1,10 @@
+"""
+Troy Brunette
+CS240
+Recursion: Least Common Denominator (LCD)
+
+Implement a solution for finding the least common denominator (LCD) of an expression using recursion.
+Pass in an integer numerator, and integer denominator value.  Your choice of language.
+
+Now compare the efficiency of each (iteration versus recursion).  What is the efficiency of each.
+"""
